557ba282d82d2e1a7547d166328e01a043b09312
kpederson3 <kpederson3@wisc.edu>
2018-10-31 17:45:48 -0500

    Trying to figure out MAX_NUMBER Assignments. Reverted back to five concurrent scans. Also default to new scan

--- Scanner_App.py
+++ Scanner_App.py
@@ -8,6 +8,9 @@ import logging
 print_lock = threading.Lock()
 
 num_threads = 0;
+threads_running = 0;
+max_assessments = (requests.get("https://api.ssllabs.com/api/v3/info").json())['maxAssessments'];
+curr_assessments = (requests.get("https://api.ssllabs.com/api/v3/info").json())['currentAssessments'];
 
 API = 'https://api.ssllabs.com/api/v3/'
 
@@ -23,10 +26,16 @@ def requestAPI(path, payload={}):
 
     try:
         response = requests.get(url, params=payload)
+        #print(response.headers)
 
     except requests.exception.RequestException:
         logging.exception('Request failed.')
         sys.exit(1)
+
+    while(response.status_code == 429):
+        print("call me icarus")
+        time.sleep(10)
+        response = requests.get(url, params=payload)
 
     data = response.json()
     return data
@@ -54,7 +63,6 @@ def newScan(host, publish='off', startNew='on', all='done', ignoreMismatch='on')
             results = requestAPI(path, payload)
     except:
         print("An error occurred")
-        return
 
     return results
 
@@ -67,15 +75,15 @@ contains information regarding the SSLLab Scan. We're just concerned with the do
 def scan_domain(domain):
 
     with print_lock:
-#       print("\nStarting thread {}".format(threading.current_thread().name))
+        print("\nStarting thread {}".format(threading.current_thread().name))
         print("Scanning " + domain)
 
     time.sleep(1)
     scan = newScan(str(domain))
 
     with print_lock:
-#        print("{}".format(threading.current_thread().name))
-#        print(scan)
+        print("{}".format(threading.current_thread().name))
+        #print(scan)
 
         try:
             ipAddress = scan['endpoints'][0]['ipAddress']
@@ -144,8 +152,15 @@ with open(str(sys.argv[1]),"r") as file:
             domain_list.append(line)
 
 
-for i in range(num_threads):
+for i in range(num_threads - 1):
+    while(max_assessments <= curr_assessments):
+        print("Max " + str(max_assessments) + " curr " + str(curr_assessments))
+        time.sleep(1)
+        max_assessments = (requests.get("https://api.ssllabs.com/api/v3/info").json())['maxAssessments']
+        curr_assessments = (requests.get("https://api.ssllabs.com/api/v3/info").json())['currentAssessments']
+
     t = threading.Thread(target=process_queue)
+    print("created thread " + str(i))
     t.daemon = True
     t.start()
     num_threads = (requests.get("https://api.ssllabs.com/api/v3/info").json())['maxAssessments']
